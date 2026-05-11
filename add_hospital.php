e2da9f4e8868279ca867b82bddc731031e7d54e1
Entity15 <abdullahayman15@gmail.com>
2026-05-12 02:13:08 +0600

    Add hospital and hospital view page

---
+++ add_hospital.php
@@ -0,0 +1,36 @@
+<?php
+include 'config.php';
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+
+    $sql = "INSERT INTO Hospital (Name, Address)
+            VALUES ('$name', '$address')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Hospital added successfully!";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Hospital</title>
+</head>
+<body>
+
+<h2>Add Hospital</h2>
+
+<form method="POST">
+    Name: <input type="text" name="name" required><br><br>
+    Address: <textarea name="address"></textarea><br><br>
+
+    <button type="submit" name="submit">Add Hospital</button>
+</form>
+
+</body>
+</html>
---
+++ view_hospitals.php
@@ -0,0 +1,34 @@
+<?php
+include 'config.php';
+
+$result = mysqli_query($conn, "SELECT * FROM Hospital");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>View Hospitals</title>
+</head>
+<body>
+
+<h2>Hospital List</h2>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Address</th>
+    </tr>
+
+    <?php while($row = mysqli_fetch_assoc($result)) { ?>
+    <tr>
+        <td><?php echo $row['Hospital_ID']; ?></td>
+        <td><?php echo $row['Name']; ?></td>
+        <td><?php echo $row['Address']; ?></td>
+    </tr>
+    <?php } ?>
+
+</table>
+
+</body>
+</html>
